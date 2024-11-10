cab5feb7102fb3b3a0dfc4b397d5a2146a38c769
estie <harin0375@gmail.com>
2024-11-10 19:30:25 +0900

    exercise03-2

--- data-engineering-practice/Exercises/Exercise-4/main.py
+++ data-engineering-practice/Exercises/Exercise-4/main.py
@@ -1,12 +1,20 @@
 import os
 import json
+import csv
 import pandas as pd
 
-def json_to_csv(json_path):
-    json_file_name = json_path.split("/")[-1]
-    print(json_file_name)
-    file_name = f"{json_file_name.split('.')[0]}.csv"
+
+def flatten(data,key,curr_row):
+    if type(data) == dict:
+        for obj in data:
+            flatten(data[obj],f"{key}.{obj}",curr_row)
+    else:
+        if not key in headers:
+            headers.append(key)
+        curr_row.append(data)
     
+    
+def json_to_csv(json_path):   
     with open(json_path,"r") as file:
         content = file.read()
         
@@ -14,9 +22,25 @@ def json_to_csv(json_path):
             print("파일이 비어있습니다")
         else:
             read_file = json.loads(content)
-            df = pd.json_normalize(read_file)
-            df.to_csv(f"./results/{file_name}")
-    
+            # df = pd.json_normalize(read_file)
+            # df.to_csv(f"./results/{file_name}")
+                
+            with open(f"results.csv", "a") as ouput_file, open(f"results.csv", "r") as check_file:
+                
+                f = csv.writer(ouput_file)
+                
+                curr_row = []
+                for obj in read_file:
+                    flatten(read_file[obj],obj,curr_row)
+
+
+                if not any(csv.reader(check_file)):
+                    print("첫번째 파일")
+                    f.writerow(headers) 
+                    
+                f.writerow(curr_row)
+                    
+            
 
 def get_jsons(path):
     files = os.listdir(f"./{path}")
@@ -31,25 +55,14 @@ def get_jsons(path):
     
 
 def main():
-    global json_files
+    global json_files, headers
     json_files = []
+    headers = []
     
-    if not (os.access("./results",os.F_OK)):
-        os.mkdir("results")
+    with open("results.csv", "w") as output:
+        f = csv.writer(output)
     
-    get_jsons("")
-
-            
-
-       
-            
-    
-    #1 json파일을 찾아서 - > 폴더 구석구석 봐야해
-    #2 csvf로 변환해서
-    #3 그럴려면 flatten out some of the nested json strucutre
-    
-    
-    
+    get_jsons("")    
     
     pass
 
